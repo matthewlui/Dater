7f62571b1c8c4a8dfeb0047d79bc75ed04485bc1
Matthew <matthewluihk@icloud.com>
2017-02-13 16:23:54 +0800

    Initial Commit

---
+++ Dater/Dater.h
@@ -0,0 +1,19 @@
+//
+//  Dater.h
+//  Dater
+//
+//  Created by Matthew Lui on 13/2/2017.
+//  Copyright © 2017 goldunderknees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dater.
+FOUNDATION_EXPORT double DaterVersionNumber;
+
+//! Project version string for Dater.
+FOUNDATION_EXPORT const unsigned char DaterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dater/PublicHeader.h>
+
+
